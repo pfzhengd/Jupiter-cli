da9ad6a6edbba9a3e959e56c246f3e1b9a4a9212
pfzheng <pengfeizheng@hotmail.com>
2021-10-16 22:38:13 +0800

    fix: 模板指定不正确。

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export default {
-  ts: 'git@github.com:pfzhengd/jupiter-js-starter.git',
-  js: 'git@github.com:pfzhengd/jupiter-ts-starter.git'
+  js: 'git@github.com:pfzhengd/jupiter-js-starter.git',
+  ts: 'git@github.com:pfzhengd/jupiter-ts-starter.git'
 }
 
 export const tempPathName = './.temp'
